2f595ce4bc3511de4f8abd49d6f4aaf588c59da4
James C Tanner <whereischason@protonmail.com>
2026-06-03 20:00:22 -0400

    Disable hyprland execution in profileExtra
    Comment out profileExtra configuration for hyprland.

--- home.nix
+++ home.nix
@@ -10,10 +10,10 @@
         shellAliases = {
             btw = "echo i use nixos, btw";
         };
-    profileExtra = ''
-      if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
-        exec hyprland
-      fi
-    '';
+    ## profileExtra = ''
+     # if [ -z "$WAYLAND_DISPLAY" ] && [ "$XDG_VTNR" = 1 ]; then
+      #  exec hyprland
+     # fi
+    #'';
   };
 }
