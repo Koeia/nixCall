39573ed9d1cd53c78b22d45073f4340dfe6b5f79
Koeia <whereischason@protonmail.com>
2026-06-06 03:29:48 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -1,65 +1,71 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
-  imports =
-    [ 
-      ./hardware-configuration.nix
+  imports = [
+    ./hardware-configuration.nix
+  ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  networking.hostName = "nixCall";
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+  services.power-profiles-daemon.enable = true;
+  services.upower.enable = true;
+
+  time.timeZone = "America/New_York";
+
+  programs.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
+  users.users.jctannu4 = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      tree
     ];
+  };
 
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    networking.hostName = "nixCall"; 
-    networking.networkmanager.enable = true;
-    hardware.bluetooth.enable = true;
-    services.power-profiles-daemon.enable = true;
-    services.upower.enable = true;
+  services.udisks2.enable = true;
+  services.tailscale.enable = true;
+  services.openssh.enable = true;
+  programs.firefox.enable = true;
+  programs.zsh.enable = true;
 
-    time.timeZone = "America/New_York";
+  environment.systemPackages = with pkgs; [
+    gcc
+    kitty
+    git
+    vim
+    wget
+    curl
+    alacritty
+    waybar
+    neovim
+    quickshell
+    kdePackages.dolphin
+    brightnessctl
+    udiskie
+    usbutils
+    yazi
+  ];
+  fonts.packages = with pkgs; [
+    nerd-fonts.jetbrains-mono
+  ];
 
-    programs.hyprland = {
-        enable = true;
-        xwayland.enable = true;
-    };
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
 
-    users.users.jctannu4 = {
-        isNormalUser = true;
-        extraGroups = [ "wheel"];
-	shell = pkgs.zsh;
-        packages = with pkgs; [
-        tree
-        ];
-    };
-    
-    services.udisks2.enable = true;
-    services.tailscale.enable = true;
-    services.openssh.enable = true ; 
-    programs.firefox.enable = true;
-    programs.zsh.enable = true;
-    
-    environment.systemPackages = with pkgs; [
-	gcc
-	kitty
-	git
-	vim
-        wget
-        curl
-        alacritty
-        waybar
-	neovim
-	quickshell
-	kdePackages.dolphin
-	brightnessctl
-	udiskie
-	usbutils	
-	yazi
-    ];  
- 	fonts.packages = with pkgs; [
-    	nerd-fonts.jetbrains-mono
- 	 ];
-	
-    nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-    system.stateVersion = "26.05"; 
+  system.stateVersion = "26.05";
 
 }
-
