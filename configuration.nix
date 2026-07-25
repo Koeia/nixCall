7b1f74a28dbe81a8d133eb187bcdd753add69c1f
Koeia <whereischason@protonmail.com>
2026-07-24 23:48:25 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -45,30 +45,6 @@
 
   environment.systemPackages = with pkgs; [
     inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default
-    #  fuzzel
-    #  kitty
-    #  git-credential-manager
-    #  libsecret
-    #  gnome-keyring
-    #  package-version-server
-    #  git
-    #  vim
-    #  wget
-    #  curl
-    #  alacritty
-    #  waybar
-    #  zed-editor
-    #  quickshell
-    #  brightnessctl
-    #  udiskie
-    #  usbutils
-    #  yazi
-    #  yaziPlugins.drag
-    #  yaziPlugins.chmod
-    #  yaziPlugins.sudo
-    #  yaziPlugins.full-border
-    #  termius
-    #  ledger-live-desktop
   ];
 
   programs.nix-ld = {
